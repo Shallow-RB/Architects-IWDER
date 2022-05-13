aedfb9be371f857ebce63251841f2388dc3f0971
Shallow-RB <ryan.bhola2601@gmail.com>
2022-05-13 18:58:56 +0200

    comment cleanup

--- js/filter.js
+++ js/filter.js
@@ -1,35 +1,4 @@
-// <!-- LETS GOOOOOOOOOOOOOOOOOOOOOOOO
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠙⠉⠁⠄⣀⣴⠄⠭⢺⢫⣯⡽⢷⣯⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠄⠄⠄⠄⠄⠄⠘⡇⣰⣄⠄⠈⢛⠻⣿⣿⣿⠌⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄⣀⣧⣟⣿⣶⠄⢾⣿⣿⣿⣿⣿⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠄⠄⠄⣤⣢⡄⠄⢩⣽⡇⢭⣯⡄⣌⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠹⣿⣂⠄⠄⠄⠄⠠⣀⠄⣿⣿⣿⣿⣿⣿⡏⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠛⠁⠉⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠉⠁⠊⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠄⠄⠄⠄⢠⡄⠄⢆⠄⠄⠄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⠄⠄⠄⠈⠄⠄⠄⠐⣿⣿⣿⣿⣿⡟⠞⣛⣗⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄⠄⠄⠄⠄⠘⠿⣿⣿⡗⠊⠁⣙⣿⣿⣿⣿⣯⡿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣵⡆⠄⠄⠄⠐⠂⠄⠄⠄⣿⣿⣷⡀⠄⠤⣭⣭⣿⣯⡳⠻⣿⡿⠮⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⠏⠉⠒⠁⠄⣰⣤⣄⣤⠄⠄⠂⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣎⠻⡿⣾⣟⠡⣏⢻⢿⣿⣿⣿⣿⣿⡿⣽⣿⣻⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣺⠋⣼⡿⡀⠄⠸⣿⣿⣿⠛⣀⣠⢾⣿⣿⣿⣿⣿⣟⠛⢿⣿⣿⡇⣻⣷⣌⠨⣿⣵⡮⣟⡹⢾⣯⣿⣿⣿⣽⣿⣏⣿⣿⣿⣿⣿
-// ⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⡿⣋⣴⡞⠄⢀⣿⢡⣷⣄⠄⠛⠛⢁⣴⣤⠥⠤⠉⡽⠷⡶⠿⠿⣿⣶⡙⣿⢡⣿⣿⣿⣷⣤⡈⢿⠏⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣟⢿⣿⢿⣿⢟⣭⣿⡿⣋⣤⣿⣿⣻⣿⣿⣿⠄⠄⠄⠈⠅⠒⠒⠒⠚⠓⣛⣛⣯⡉⢍⣽⣷⠏⣸⣿⣿⡿⠉⣉⠁⠄⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⡟⣰⣿⢜⣷⣿⣿⣯⣴⣿⣿⣿⣛⣻⣛⣻⣿⣧⡀⠁⠄⢅⢈⢾⣿⣿⣿⣟⣻⡿⣷⣿⣿⡟⣰⣿⣿⣿⣿⡄⠉⠁⣠⣿⣿⡿⣿⣻⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⣧⣿⡝⢟⢝⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣤⠄⠁⠈⢿⢿⣿⣿⣿⣿⣾⣿⣿⡟⠄⢾⣿⣿⣿⣿⡀⡀⢀⣻⣿⣿⡿⣾⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿
-// ⣿⣿⣿⣿⢾⣪⣄⣵⣭⡦⠟⣣⢿⣿⣿⣿⣧⣺⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠈⣉⣉⣭⣽⣿⣿⡿⠄⠄⢨⣿⣿⣿⣿⣿⣷⣶⣤⣌⠸⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿
-// ⣿⣿⣿⣿⣹⣿⣿⣿⣾⣿⣥⣿⣾⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡿⡁⠄⠄⠄⠓⠿⠿⢿⠿⡿⠋⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⡙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
-// ⣿⣿⣿⣿⢟⣾⣿⣿⣿⣿⣭⣽⣶⣾⣟⣿⣾⣷⣿⣿⣿⣿⣿⣿⣵⣄⡀⠄⠄⠄⠄⠄⠄⠄⠄⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣜⢿⣿⡟⣿⣿⣾⣿⣿⣻
-// ⣿⣿⣿⢷⣴⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢺⣿⣿⢟⣿⣿⣿⣿⣟
-// ⣿⣿⣿⠄⠜⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⠿⠾⠿⣻⣽⣿⣿⡿
-// ⣿⣿⡿⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠒⢲⣿⣿⣿⣾
-// ⣿⣟⠳⣌⡀⣘⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣳⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠠⠟⣼⣽⣿⣿⣿
-// ⣿⠏⠉⠪⢿⣻⠻⣿⡿⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⡳⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠐⠹⠽⠽⣿⣿⣿
-// ⣿⠄⠄⠄⣢⡠⣫⣽⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣟⢳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢀⣤⣽⣿⣿⣿⣿
-// ⡏⠄⠄⠠⡝⣿⢶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⡿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣫⢧⠴⢚⠄⠩⠽⣿⣻⣿⣿
-// ⠄⠄⠢⣀⠙⡤⡤⣫⣿⣹⣯⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣏⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⣈⡰⠄⠄⠄⣤⣭⣿⣝⣿⣿ -->
+
 const Music = document.querySelectorAll(['section[data-category="Music"]']);
 const Clothing = document.querySelectorAll(['section[data-category="Clothing"]']);
 const Headwear = document.querySelectorAll(['section[data-category="Headwear"]']);
@@ -46,53 +15,26 @@ checkMusic.checked = true;
 checkPoster.checked = true;
 
 checkMusic.onclick = function () {
-  checkMusic.checked == true
+  checkMusic.checked === true
 	? Music.forEach((elem) => {elem.style.display = "";})
 	: Music.forEach((elem) => {elem.style.display = "none";});
   };
 
 
 checkClothing.onclick = function () {
-	checkClothing.checked == true
+	checkClothing.checked === true
 	? Clothing.forEach((elem) => {elem.style.display = "";})
 	: Clothing.forEach((elem) => {elem.style.display = "none";});
 	};
 
 checkHeadwear.onclick = function () {
-	checkHeadwear.checked == true
+	checkHeadwear.checked === true
 	? Headwear.forEach((elem) => {elem.style.display = "";})
 	: Headwear.forEach((elem) => {elem.style.display = "none";});
 	};
 
 checkPoster.onclick = function () {
-	checkPoster.checked == true
+	checkPoster.checked === true
 	? Poster.forEach((elem) => {elem.style.display = "";})
 	: Poster.forEach((elem) => {elem.style.display = "none";});
 	};
-
-// var checks = document.querySelectorAll("input[type=checkbox]:checked");
-// if (checks.length === 0) {
-//   for (let j = 0; j < cards.length; j++) {
-//     cards[j].style.display = "";
-//   }
-// }
-
-// if (
-//   checkMusic.checked == false &&
-//   checkClothing.checked == false &&
-//   checkHeadwear.checked == false &&
-//   checkPoster.checked == false
-// ) {
-//   catMusic.forEach((elem) => {
-//     elem.style.display = "";
-//   });
-//   catClothing.forEach((elem) => {
-//     elem.style.display = "";
-//   });
-//   catHeadwear.forEach((elem) => {
-//     elem.style.display = "";
-//   });
-//   catPoster.forEach((elem) => {
-//     elem.style.display = "";
-//   });
-// }
