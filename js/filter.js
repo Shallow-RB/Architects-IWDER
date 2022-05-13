740b9d189f05a81fcd9746b9ae0604bdd559991d
Shallow-RB <ryan.bhola2601@gmail.com>
2022-05-13 18:40:02 +0200

    Initial commit

---
+++ js/filter.js
@@ -0,0 +1,98 @@
+// <!-- LETS GOOOOOOOOOOOOOOOOOOOOOOOO
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠛⠙⠉⠁⠄⣀⣴⠄⠭⢺⢫⣯⡽⢷⣯⡻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠄⠄⠄⠄⠄⠄⠘⡇⣰⣄⠄⠈⢛⠻⣿⣿⣿⠌⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⠄⠄⠄⠄⠄⣀⣧⣟⣿⣶⠄⢾⣿⣿⣿⣿⣿⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠄⠄⠄⣤⣢⡄⠄⢩⣽⡇⢭⣯⡄⣌⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠹⣿⣂⠄⠄⠄⠄⠠⣀⠄⣿⣿⣿⣿⣿⣿⡏⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠛⠁⠉⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠉⠉⠉⠁⠊⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠁⠄⠄⠄⠄⢠⡄⠄⢆⠄⠄⠄⣀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣤⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⣀⠄⠄⠄⠈⠄⠄⠄⠐⣿⣿⣿⣿⣿⡟⠞⣛⣗⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡀⠄⠄⠄⠄⠄⠄⠄⠘⠿⣿⣿⡗⠊⠁⣙⣿⣿⣿⣿⣯⡿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⣵⡆⠄⠄⠄⠐⠂⠄⠄⠄⣿⣿⣷⡀⠄⠤⣭⣭⣿⣯⡳⠻⣿⡿⠮⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠉⠏⠉⠒⠁⠄⣰⣤⣄⣤⠄⠄⠂⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣇⣎⠻⡿⣾⣟⠡⣏⢻⢿⣿⣿⣿⣿⣿⡿⣽⣿⣻⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣺⠋⣼⡿⡀⠄⠸⣿⣿⣿⠛⣀⣠⢾⣿⣿⣿⣿⣿⣟⠛⢿⣿⣿⡇⣻⣷⣌⠨⣿⣵⡮⣟⡹⢾⣯⣿⣿⣿⣽⣿⣏⣿⣿⣿⣿⣿
+// ⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⡿⣋⣴⡞⠄⢀⣿⢡⣷⣄⠄⠛⠛⢁⣴⣤⠥⠤⠉⡽⠷⡶⠿⠿⣿⣶⡙⣿⢡⣿⣿⣿⣷⣤⡈⢿⠏⢼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣟⢿⣿⢿⣿⢟⣭⣿⡿⣋⣤⣿⣿⣻⣿⣿⣿⠄⠄⠄⠈⠅⠒⠒⠒⠚⠓⣛⣛⣯⡉⢍⣽⣷⠏⣸⣿⣿⡿⠉⣉⠁⠄⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⡟⣰⣿⢜⣷⣿⣿⣯⣴⣿⣿⣿⣛⣻⣛⣻⣿⣧⡀⠁⠄⢅⢈⢾⣿⣿⣿⣟⣻⡿⣷⣿⣿⡟⣰⣿⣿⣿⣿⡄⠉⠁⣠⣿⣿⡿⣿⣻⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣧⣿⡝⢟⢝⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣤⠄⠁⠈⢿⢿⣿⣿⣿⣿⣾⣿⣿⡟⠄⢾⣿⣿⣿⣿⡀⡀⢀⣻⣿⣿⡿⣾⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿
+// ⣿⣿⣿⣿⢾⣪⣄⣵⣭⡦⠟⣣⢿⣿⣿⣿⣧⣺⣿⣿⣿⣿⣿⣿⠄⠄⠄⠄⠈⣉⣉⣭⣽⣿⣿⡿⠄⠄⢨⣿⣿⣿⣿⣿⣷⣶⣤⣌⠸⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿
+// ⣿⣿⣿⣿⣹⣿⣿⣿⣾⣿⣥⣿⣾⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡿⡁⠄⠄⠄⠓⠿⠿⢿⠿⡿⠋⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⡙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⢟⣾⣿⣿⣿⣿⣭⣽⣶⣾⣟⣿⣾⣷⣿⣿⣿⣿⣿⣿⣵⣄⡀⠄⠄⠄⠄⠄⠄⠄⠄⢀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣜⢿⣿⡟⣿⣿⣾⣿⣿⣻
+// ⣿⣿⣿⢷⣴⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢺⣿⣿⢟⣿⣿⣿⣿⣟
+// ⣿⣿⣿⠄⠜⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⠿⠾⠿⣻⣽⣿⣿⡿
+// ⣿⣿⡿⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠄⠄⠒⢲⣿⣿⣿⣾
+// ⣿⣟⠳⣌⡀⣘⣛⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⣿⣿⣳⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠠⠟⣼⣽⣿⣿⣿
+// ⣿⠏⠉⠪⢿⣻⠻⣿⡿⣵⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⡳⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⠐⠹⠽⠽⣿⣿⣿
+// ⣿⠄⠄⠄⣢⡠⣫⣽⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⣿⣟⢳⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢀⣤⣽⣿⣿⣿⣿
+// ⡏⠄⠄⠠⡝⣿⢶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣿⣿⡿⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⣫⢧⠴⢚⠄⠩⠽⣿⣻⣿⣿
+// ⠄⠄⠢⣀⠙⡤⡤⣫⣿⣹⣯⠙⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣏⣿⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⣈⡰⠄⠄⠄⣤⣭⣿⣝⣿⣿ -->
+const Music = document.querySelectorAll(['section[data-category="Music"]']);
+const Clothing = document.querySelectorAll(['section[data-category="Clothing"]']);
+const Headwear = document.querySelectorAll(['section[data-category="Headwear"]']);
+const Poster = document.querySelectorAll(['section[data-category="Poster"]']);
+
+const checkMusic = document.getElementById("Music");
+const checkClothing = document.getElementById("Clothing");
+const checkHeadwear = document.getElementById("Headwear");
+const checkPoster = document.getElementById("Poster");
+
+checkClothing.checked = true;
+checkHeadwear.checked = true;
+checkMusic.checked = true;
+checkPoster.checked = true;
+
+checkMusic.onclick = function () {
+  checkMusic.checked == true
+	? Music.forEach((elem) => {elem.style.display = "";})
+	: Music.forEach((elem) => {elem.style.display = "none";});
+  };
+
+
+checkClothing.onclick = function () {
+	checkClothing.checked == true
+	? Clothing.forEach((elem) => {elem.style.display = "";})
+	: Clothing.forEach((elem) => {elem.style.display = "none";});
+	};
+
+checkHeadwear.onclick = function () {
+	checkHeadwear.checked == true
+	? Headwear.forEach((elem) => {elem.style.display = "";})
+	: Headwear.forEach((elem) => {elem.style.display = "none";});
+	};
+
+checkPoster.onclick = function () {
+	checkPoster.checked == true
+	? Poster.forEach((elem) => {elem.style.display = "";})
+	: Poster.forEach((elem) => {elem.style.display = "none";});
+	};
+
+// var checks = document.querySelectorAll("input[type=checkbox]:checked");
+// if (checks.length === 0) {
+//   for (let j = 0; j < cards.length; j++) {
+//     cards[j].style.display = "";
+//   }
+// }
+
+// if (
+//   checkMusic.checked == false &&
+//   checkClothing.checked == false &&
+//   checkHeadwear.checked == false &&
+//   checkPoster.checked == false
+// ) {
+//   catMusic.forEach((elem) => {
+//     elem.style.display = "";
+//   });
+//   catClothing.forEach((elem) => {
+//     elem.style.display = "";
+//   });
+//   catHeadwear.forEach((elem) => {
+//     elem.style.display = "";
+//   });
+//   catPoster.forEach((elem) => {
+//     elem.style.display = "";
+//   });
+// }
